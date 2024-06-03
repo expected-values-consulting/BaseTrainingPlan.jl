30e12b7695f1af47b1577a6068a448ffc4de78a6
Documenter.jl <documenter@juliadocs.github.io>
2024-06-03 23:09:33 +0000

    build based on f084a4d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BaseTrainingPlan","category":"page"},{"location":"#BaseTrainingPlan","page":"Home","title":"BaseTrainingPlan","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BaseTrainingPlan.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BaseTrainingPlan]","category":"page"}]
+}
