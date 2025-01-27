03dfcfa1e21f1db163f748d635ef44abfc1acffe
Mr <jonathan.miller@fieldofnodes.com>
2025-01-27 02:32:01 +0000

    Update BaseTrainingPlan.jl

--- src/BaseTrainingPlan.jl
+++ src/BaseTrainingPlan.jl
@@ -9,7 +9,7 @@ module BaseTrainingPlan
         generate_modules_from_training_assessment
 
 
-    function test_training_assessment_file(df::DataFrame)
+    function _test_training_assessment_file(df::DataFrame)
         @assert length(names(df)) == 2 "Number of columns is not as expected."
         @assert names(df) == ["Index", "Title"] "Column names are not as expected."
         @assert all([i isa String for i in df.Title]) "Not all values in Skill column are strings."
@@ -18,13 +18,13 @@ module BaseTrainingPlan
 
     function read_training_assessment_file(file_name)
         assessment_file = CSV.read(file_name,DataFrame, header=true)
-        test_training_assessment_file(assessment_file)
+        _test_training_assessment_file(assessment_file)
         assessment_file
     end
 
     function generate_modules_from_training_assessment(file_name)
         assessment_file = CSV.read(file_name,DataFrame, header=true)
-        test_training_assessment_file(assessment_file)
+        _test_training_assessment_file(assessment_file)
         assessment_file.Title
     end
 
