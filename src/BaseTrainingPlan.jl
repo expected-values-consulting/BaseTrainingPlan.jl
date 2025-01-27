31ef7fdc7395a4986a8e3e7cb3113dec2e5ba683
Mr <jonathan.miller@fieldofnodes.com>
2025-01-27 02:31:09 +0000

    Update BaseTrainingPlan.jl

--- src/BaseTrainingPlan.jl
+++ src/BaseTrainingPlan.jl
@@ -10,14 +10,10 @@ module BaseTrainingPlan
 
 
     function test_training_assessment_file(df::DataFrame)
-        @assert length(names(df)) == 4 "Number of columns is not as expected."
-        @assert names(df) == ["Number", "Skill", "Current_skills", "Desired_skill"] "Column names are not as expected."
-        @assert all([i isa String for i in df.Skill]) "Not all values in Skill column are strings."
-        @assert all([!isempty(i) for i in df.Skill]) "Skill desciprtion in Skill column is empty."
-        @assert all([i isa Number for i in df.Current_skills]) "Not all values in Current_skills column are numbers."
-        @assert all([i ∈ 0:5   for i in df.Current_skills]) "Not all values in Current_skills column are in 0:5."
-        @assert all([i isa Number for i in df.Desired_skill]) "Not all values in Desired_skill column are numbers."
-        @assert all([i ∈ 0:5   for i in df.Desired_skill]) "Not all values in Desired_skill column are in 0:5."
+        @assert length(names(df)) == 2 "Number of columns is not as expected."
+        @assert names(df) == ["Index", "Title"] "Column names are not as expected."
+        @assert all([i isa String for i in df.Title]) "Not all values in Skill column are strings."
+        @assert all([!isempty(i) for i in df.Title]) "Skill desciprtion in Skill column is empty."
     end
 
     function read_training_assessment_file(file_name)
@@ -29,7 +25,7 @@ module BaseTrainingPlan
     function generate_modules_from_training_assessment(file_name)
         assessment_file = CSV.read(file_name,DataFrame, header=true)
         test_training_assessment_file(assessment_file)
-        assessment_file.Skill
+        assessment_file.Title
     end
 
 end
